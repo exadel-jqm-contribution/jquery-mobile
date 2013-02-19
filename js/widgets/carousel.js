16a5fbec4de489ffcb8971c16d295e025d186c51
Anton Artyukh <jqm_contribution@exadel.com>
2013-02-19 13:03:04 +0000

    carousel: refactoring, change layout and css, tests update

--- js/widgets/carousel.js
+++ js/widgets/carousel.js
@@ -8,309 +8,257 @@
 define( ["jquery", "../jquery.mobile.widget" ], function ( $ ) {
 //>>excludeEnd( "jqmBuildExclude" );
 
-	(function ( $, undefined ) {
-		$.widget( "mobile.carousel", $.mobile.widget, {
-			options: {
-				viewPortWidth: 0,
-				viewPortHeight: 0,
-				showLinks: true,
-				linksBefore: true,
-				linksEnabled: true,
-				linksSize: 40,
-				cycle: true,
-				startFrom: 0,
-				animationDuration: 240,
-				titleTag: "span"
-			},
-			_cursor: 0,
-			_cursor_next: 1,
-			_nextItem: false,
-			_items: [],
-			_links: null,
-			_loaded_items: 0,
-			_create: function() {
-				this.element.addClass( "ui-carousel" );
 
-				this.options = $.extend( this.options, this.element.data( "options" ) );
+(function ( $, undefined ) {
+	$.widget( "mobile.carousel", $.mobile.widget, {
+		options:{
+			indicators: null,
+			indicatorsListClass: "ui-carousel-indicators",
+			animationDuration: 250,
+			showIndicator: true,
+			showTitle: true,
+			createIndicator: null,
+			titleBuildIn: false,
+			createTitle: null,
+			enabled: true,
+			depended: false
+		},
+		_list: null,
+		_create: function() {
+			this.element.addClass( "ui-carousel" );
+			this._list = $( ".ui-carousel-items", this.element );
+			this.options = $.extend( this.options, this.element.data( "options" ) );
+			this.options = $.extend( this.options, this.element.data() );
 
-				if (this.options.viewPortWidth === 0) {
-					this.options.viewPortWidth = this.element.width();
-				}
-				if (this.options.viewPortHeight === 0) {
-					this.options.viewPortHeight =
-						$.mobile.getScreenHeight() - (this.options.showLinks ? this.options.linksSize : 0);
+			if ( this.options.showIndicator !== false ) {
+				if ( this.options.indicators === null ) {
+					this.options.indicators = $('<div></div>');
+					this.options.indicators.appendTo(this.element);
+				} else if (typeof this.options.indicators === "string") {
+					this.options.indicators = $(this.options.indicators);
 				}
 
-				this.element.css( "min-height", this.options.viewPortHeight );
-
-				this._links = $( "<div> </div>" );
-				if (this.options.showLinks) {
-					this._links.addClass( "ui-carousel-links" );
-					if (this.options.linksBefore) {
-						this._links.prependTo( this.element );
-					} else {
-						this._links.appendTo( this.element );
-					}
-				}
-
-				this._bindEvents();
-
-				this.refresh();
-				this.moveTo( this.options.startFrom || 0 );
-			},
-
-			_bindEvents: function() {
-				this.element.on( "swipeleft", this.next.bind(this) );
-				this.element.on( "swiperight", this.prev.bind(this) );
-			},
-
-			_UID: function() {
-				return this.uuid + "-" + this._items.length;
-			},
-
-			_wraperBox: function ( box_width_str ) {
-				var box = $( "<div />" );
-				box.addClass( "ui-carousel-box" );
-				box.css( "max-width", box_width_str );
-				return box;
-			},
-
-			_onCompleteImageLoad: function ( self, $li ) {
-				if (this.width > this.height) {
-					this.style.width = "95%";
-				} else {
-					this.style.height = "90%";
-				}
-				var box = self._wraperBox( this.width );
-				if ($li.data( "title" )) {
-					var title = $( "<" + self.options.titleTag + " />" ).addClass( "ui-carousel-item-title" );
-					title.text( $li.data("title") );
-					title.appendTo( box );
-				}
-				$( this ).appendTo( box );
-				$li.html( '' );
-				box.appendTo( $li );
-				self._loaded_items++;
-				$li.trigger( "ready" );
-				self._trigger( "itemadd", {
-					itemType: "image",
-					el: $li
-				});
-				self._trigger( "itemimageadd", {
-					el: $li
-				});
-			},
-
-			_onFailureImageLoad: function ( imageUrl, $li, type ) {
-				this._trigger( "failload", {
-					itemType: "image",
-					item: imageUrl
-				});
-				this._trigger( "failimageload", {
-					item: imageUrl
-				});
-				$li.trigger( "failure", {
-					type: type
-				});
-			},
-
-			_image_add: function ( $li ) {
-				var img = new Image(),
-					imageUrl = $li.data( "imageUrl" );
-				img.onload = this._onCompleteImageLoad.bind( img, this, $li );
-				img.onerror = this._onFailureImageLoad.bind( this, imageUrl, $li, "error" );
-				img.onabort = this._onFailureImageLoad.bind( this, imageUrl, $li, "abort" );
-				img.src = imageUrl;
-			},
-
-			_raw_html_add: function ( $li ) {
-				var $box = this._wraperBox( $li.width() );
-
-				$( "*", $li ).detach().appendTo( $box );
-				$box.appendTo( $li );
-				$li.trigger( "ready" );
-				this._loaded_items++;
-			},
-
-			render_slide: function ( i, li ) {
-				var $li = $( li ),
-					mode = $li.data();
-
-				mode.itemType = mode.itemType || "html";
-
-				var UUID = mode.itemType + this._UID();
-
-				$li.addClass( "ui-carousel-item" );
-				$li.attr("id", "item" + UUID);
-
-				this._add_link( $li, UUID );
-
-				switch ( mode.itemType ) {
-					case "image":
-						this._image_add( $li );
-						break;
-
-					case "html":
-						this._raw_html_add( $li );
-						break;
-				}
-
-				$li.addClass('ui-carousel-' + mode.itemType);
-
-				this._items.push( {
-					item: $li,
-					itemType: mode.itemType
-				});
-			},
-
-			_add_link: function( $li, uuid) {
-				var link = $( "<a>&nbsp;</a>" ),
-					link_id = "link" + uuid;
-				link.addClass( "ui-carousel-link" ).addClass( "ui-carousel-item-loading" );
-				if ( $li.data( "href" ) ) {
-					link.attr( "href", $li.data("href") );
-				}
-
-				link.attr( "id", link_id );
-
-				$li.on( "ready", {
-					link: "#" + link_id
-				}, $.proxy( function( event ) {
-					$( event.data.link ).removeClass( "ui-carousel-item-loading" );
-				}, this));
-
-				$li.on( "show", {
-					link: "#" + link_id
-				}, $.proxy( function( event) {
-					$( event.data.link ).addClass( "ui-carousel-link-selected" );
-				}, this));
-
-				if (this.options.linksEnabled) {
-					link.on("click", this.moveTo.bind(this, this._items.length));
-				}
-
-				link.appendTo( this._links );
-			},
-
-			_animate: function( direction, next_item, onReady) {
-				var current_item = $("li.ui-carousel-show", this.element);
-				if (current_item.length === 0) {
-					current_item = $("li.ui-carousel-item:first", this.element);
-					current_item.css("right", "0%");
-					onReady(null, next_item);
-					return;
-				}
-
-				switch (direction) {
-				case "next":
-					next_item.css("right", "-100%");
-					$( current_item ).animate({
-						right: "100%"
-					}, {
-						duration: this.options.animationDuration,
-						complete: onReady.bind(this, current_item, next_item),
-						step: function( now, fx ) {
-							next_item.css("right", (now - 100) + "%");
-						}
-					});
-					break;
-				case "prev":
-					next_item.css("right", "100%");
-					$( current_item ).animate({right: "-100%"}, {
-						duration: this.options.animationDuration,
-						complete: onReady.bind(this, current_item, next_item),
-						step: function( now, fx) {
-							next_item.css("right", (100 + now) + "%");
-						}
-					});
-					break;
-				default:
-					next_item.css("right", "-100%");
-					$( current_item ).animate({right: "100%"}, {
-						duration: this.options.animationDuration,
-						complete: onReady.bind(this, current_item, next_item),
-						step: function( now, fx) {
-							next_item.css("right", (100 - now) + "%");
-						}
-					});
-					break;
-				}
-			},
-
-			_process_index: function( index ) {
-				if (this.options.cycle) {
-					index = index > this._loaded_items ? 0 : index;
-					index = index < 0 ? this._loaded_items.length : index;
-				} else {
-					index = index > this._loaded_items ? this._loaded_items - 1 : index;
-					index = index < 0 ? 0 : index;
-				}
-				return index;
-			},
-
-			moveTo: function( index, direction, event) {
-				if (index === undefined) {
-					index = this._cursor_next;
-				}
-
-				this._cursor = this._process_index(index);
-
-				var next_item = this.currentFrame();
-
-				this._animate( direction, next_item, $.proxy( function( prev_item, next_item ) {
-					$( "li.ui-carousel-item", this.element ).removeClass( "ui-carousel-show" );
-					next_item.addClass( "ui-carousel-show" );
-					this._links.find( ".ui-carousel-link" ).removeClass( "ui-carousel-link-selected" );
-					this._trigger( "moveto", "moveto", this._cursor );
-					if ( prev_item ) {
-						prev_item.trigger( "hide" );
-					}
-					next_item.trigger( "show" );
-				}, this ) );
-			},
-
-			next: function() {
-				this._cursor_next = this._cursor + 1;
-				this.moveTo( this._cursor_next, "next" );
-				this._trigger( "stepnext", "stepnext", this._cursor );
-			},
-
-			prev: function() {
-				this._cursor_next = this._cursor - 1;
-				this.moveTo( this._cursor_next, "prev" );
-				this._trigger( "stepprev", this._cursor );
-			},
-
-			currentIndex: function() {
-				return this._cursor;
-			},
-
-			currentFrame: function() {
-				return this._items[this._cursor].item;
-			},
-
-			frame: function( index ) {
-				index = this._process_index( index );
-				return this._items[ index ].item;
-			},
-
-			setNextFrame: function( frame) {
-				if ( typeof frame == "number" ) {
-					this._cursor_next = frame - 1;
-				}
-			},
-
-			refresh: function() {
-				$( "li[data-item-type]", this.element ).each( $.proxy( function( index, el ) {
-					this.render_slide( index, el );
-				}, this ) );
+				this.options.indicators.addClass(this.options.indicatorsListClass);
 			}
-		});
-		$( document ).bind( "pageshow", function( e ) {
-			$( document ).trigger( "ui-carouselbeforecreate" );
-			return $( ":jqmData(role='carousel')", e.target ).carousel();
-		});
-	})( jQuery );
+
+			if ( this.options.createIndicator === null ) {
+				this.options.createIndicator = this._createIndicator.bind(this);
+			}
+
+			if ( this.options.createTitle === null ) {
+				this.options.createTitle = this._create_title.bind(this);
+			}
+
+			this._bindEvents();
+			this.refresh();
+			this.to(0);
+		},
+
+		refresh: function() {
+			$( "*[data-type]", this._list ).each( $.proxy( this._render_frame, this ) );
+		},
+
+		_bindEvents: function() {
+			if ( this.options.depended ) {
+				return;
+			}
+			this.element.on( "swipeleft", this.next.bind(this) );
+			this.element.on( "swiperight", this.previous.bind(this) );
+			this.element.find( ".ui-right-arrow" ).on('click', this.next.bind(this));
+			this.element.find( ".ui-left-arrow" ).on('click', this.previous.bind(this));
+		},
+
+		_wraperBox: function( el, title) {
+			var box = $( "<div></div>" );
+			box.addClass( "ui-carousel-box" );
+			box.appendTo( el );
+
+			this.options.showTitle && this._create_title( title, el );
+
+			return box;
+		},
+
+		_create_title: function( title_str, target ) {
+			var title = $( "<div></div>" ),
+				inside = null;
+			title.addClass( "ui-carousel-title" );
+			if ( this.options.titleBuildIn ) {
+				inside = $( "<div></div>" );
+				inside.addClass( "ui-carousel-title-inside" );
+				inside.text(title_str);
+				inside.appendTo(title);
+			} else {
+				title.text(title_str);
+			}
+			title.appendTo( target );
+			return title;
+		},
+
+		_load_image: function( url, target, parent ) {
+			var img = new Image(),
+				error = function () {};
+			img.onload = function() {
+				var $img = $(this);
+				$img.addClass( "ui-carousel-content" );
+				$img.appendTo( target );
+				parent.trigger( "ready", {item: parent} );
+			};
+
+			img.onerror = error;
+			img.onabort = error;
+			img.src = url;
+		},
+
+		_load_html: function( $el, title ) {
+			var content = $el.html(),
+				item;
+			$el.html( "" );
+			item = this._wraperBox( $el, title );
+			item.html(content);
+			$el.trigger( "ready" );
+		},
+
+		add: function( type, title, content, onShow) {
+			var el = $( "<div></div>" ),
+				imageUrl = type == "image" ? content : "";
+
+			el.data( {
+				type: type,
+				title: title,
+				imageUrl: imageUrl
+			});
+			el.html( (type == "image" ? "" : content) );
+			el.appendTo( this._list );
+			onShow && el.on( "show", onShow );
+			this._render_frame( this._list.length-1, el);
+			return el;
+		},
+
+		next: function() {
+			this.slide( "next" );
+		},
+
+		previous: function() {
+			this.slide( "prev" );
+		},
+
+		slide: function( type, next ) {
+			var $active = this.element.find( ".ui-carousel-item.ui-carousel-active" ),
+				$next = next || $active[type]( ".ui-carousel-item" );
+
+			if ( type !== "next" && type !== "prev" ) {
+				type = $next.nextAll(".ui-carousel-active").length == 0 ? "next" : "prev";
+			}
+
+			var	direction = type == "next" ? 1 : -1,
+				fallback = type == "next" ? "first" : "last";
+
+			if ( $active.length === 0 ) {
+				$next.trigger( "show" );
+				return;
+			}
+
+			if ( $next.hasClass( "ui-carousel-active" ) ) {
+				return;
+			}
+
+			$next = $next.length ? $next : this.element.find( ".ui-carousel-item" )[fallback]();
+
+			$next.trigger( "beforeshow" );
+
+			$next.css( "left", ( 100 * direction ) + '%' );
+			$active.animate( {
+				left: ( 100 * direction * -1 ) + '%'
+			}, {
+				duration: this.options.animationDuration,
+				complete: function() {
+					$active.trigger( "hide" );
+					$next.trigger( "show" );
+				},
+				step: function( now, fx ) {
+					$next.css( "left", (100 * direction + now) + "%" );
+				}
+			});
+		},
+
+		to: function ( index ) {
+			var $el = $( ".ui-carousel-item:eq(" + index + ")", this.element );
+			this.slide( null, $el );
+		},
+
+		getFrame: function ( index ) {
+			var f = $( ".ui-carousel-item:eq(" + index + ")", this.element );
+			if ( f.length === 0 ) {
+				return false;
+			}
+			return f;
+		},
+
+		_render_frame: function( index, el ) {
+			var $el = $( el ),
+				params = $el.data(),
+				item, indicator;
+			$el.addClass( "ui-carousel-item" );
+
+			switch ( params.type ) {
+				case "image":
+					item = this._wraperBox( $el, params.title || "''" );
+					this._load_image( params.imageUrl, item, $el );
+					break;
+				case "html":
+					this._load_html( $el, params.title || "" );
+					break;
+			}
+			if ( this.options.showIndicator) {
+				indicator = this._createIndicator( this.options.indicators, params.title || "" );
+				indicator.data( "slideTo", index );
+
+				if ( !this.options.depended ) {
+					indicator.on( "click", {
+						move: this.to.bind( this )
+					}, function ( event ) {
+						var n = $( this ).data( "slideTo" );
+						event.data.move( n );
+					});
+				}
+
+				indicator.on( "show", function( event ) {
+					$( this ).addClass('ui-carousel-indicator-active');
+				});
+				indicator.on( "hide", function( event ) {
+					$( this ).removeClass('ui-carousel-indicator-active');
+				});
+				$el.data( "indicator", indicator.get(0) );
+				$el.on( "hide", function( event ) {
+					$($( this ).data( "indicator" )).trigger( "hide" );
+				});
+				$el.on( "show" , function( event ) {
+					$($( this ).data( "indicator" )).trigger( "show" );
+				});
+			}
+
+			$el.on( "hide", function( event ) {
+				$( this ).removeClass( "ui-carousel-active" );
+			});
+			$el.on( "show" , function( event ) {
+				$( this ).addClass( "ui-carousel-active" );
+			});
+		},
+
+		_createIndicator: function( list, title) {
+			var indicator = $( "<div></div>" );
+			indicator.addClass( "ui-carousel-indicator" );
+			indicator.attr( "title", title );
+			indicator.appendTo( list );
+			return indicator;
+		}
+	});
+	$( document ).bind( "pageshow", function( e ) {
+		$( document ).trigger( "ui-carouselbeforecreate" );
+		return $( ":jqmData(role='carousel')", e.target ).carousel();
+	});
+})( jQuery );
+
 //>>excludeStart("jqmBuildExclude", pragmas.jqmBuildExclude);
 });
 //>>excludeEnd( "jqmBuildExclude" );
--- tests/unit/carousel/carousel_core.js
+++ tests/unit/carousel/carousel_core.js
@@ -2,56 +2,55 @@
  * mobile carousel unit tests
  */
 (function($){
-	module( "jquery.mobile.carousel.js" );
-	var c = $( "#carousel" );
-	var animation_time = 30;
+	module( "query.mobile.carousel.js" );
 
-	test( "widget must load 2 images and 2 text blocks", function() {
-		ok( $("li.ui-carousel-image", c ).length == 2, "2 images" );
-		ok( $("li.ui-carousel-html", c ).length == 2, "2 text blocks" );
-	});
+	var animation_time = 160;
 
 	asyncTest( "go to next slides", function() {
-		expect( 3 );
-		c.carousel( "next" );
-		setTimeout( function () {
-			ok( c.carousel("currentIndex") == 1, "after first NEXT, cursor must by on second slide" );
-			ok( $("li.ui-carousel-item:eq(1)", c).hasClass("ui-carousel-show"), "current slide must have class .ui-carousel-show" );
-			ok( $("li.ui-carousel-show", c).length === 1, "only one item has .ui-carousel-show" );
+		expect( 2 );
+		var c = $( "#carousel" ),
+			$frame = c.carousel( "getFrame", 1 );
+		$frame.on( "show", function() {
+			ok( $(".ui-carousel-item:eq(1)", c).hasClass("ui-carousel-active"), "current slide must have class .ui-carousel-active" );
+			ok( $(".ui-carousel-active", c).length === 1, "only one item has .ui-carousel-active" );
 		 	start();
-		}, animation_time);
+		});
+		c.carousel( "next" );
 	});
 
-	asyncTest( "go to prev slides", function() {
-		expect( 3 );
-		c.carousel( "prev" );
-		setTimeout( function () {
-			ok( c.carousel("currentIndex") == 0, "after first PREV, cursor must by on first slide" );
-			ok( $("li.ui-carousel-item:eq(0)", c).hasClass("ui-carousel-show"), "current slide must have class .ui-carousel-show" );
-			ok( !$("li.ui-carousel-item:eq(1)", c).hasClass("ui-carousel-show"), "second slide can't have class .ui-carousel-show" );
+	asyncTest( "go to prev (LAST) slide", function() {
+		var c = $( "#carousel2" );
+		expect( 2 );
+		var $frame = c.carousel( "getFrame", c.carousel( "length" ) - 1 );
+		$frame.on( "show", function() {
+			ok( $(".ui-carousel-item:last", c).hasClass("ui-carousel-active"), "current slide must have class .ui-carousel-active" );
+			ok( !$(".ui-carousel-item:first", c).hasClass("ui-carousel-active"), "second slide can't have class .ui-carousel-active" );
 			start();
-		}, animation_time);
+		});
+		c.carousel( "previous" );
 	});
 
 	asyncTest( "go to specific slide", function() {
-		expect( 2 );
-		c.carousel( 'moveTo', 3 );
-		setTimeout( function () {
-			ok( c.carousel("currentIndex") === 3, "current slide == 4" );
-			ok( $("li.ui-carousel-item:eq(3)", c).hasClass("ui-carousel-show"), "current slide (4) must have class .ui-carousel-show" );
-			start();
-		}, animation_time * 2); // we have 2 moves, both have animation.
-	});
-
-	asyncTest( "wait show event for specific slide", function() {
+		var c = $( "#carousel3" );
 		expect( 1 );
-		c.carousel('moveTo', 0);
-		var frame = c.carousel('frame', 3);
-		frame.on( 'show', function(event) {
-			ok(true, 'fire show event');
+		var $frame = c.carousel( "getFrame", 3 );
+		$frame.on( "show", function() {
+			ok( $(".ui-carousel-item:eq(3)", c).hasClass("ui-carousel-active"), "current slide (4) must have class .ui-carousel-active" );
 			start();
 		});
-		c.carousel('moveTo', 3);
+		c.carousel( "to", 3 );
+	});
+
+	asyncTest( "wait hide event for specific slide", function() {
+		expect( 1 );
+		var c = $( "#carousel4" );
+		var frame = c.carousel('getFrame', 0);
+		c.carousel( "to", 0);
+		frame.on( "hide", function(event) {
+			ok(true, "fire HIDE event");
+			start();
+		});
+		c.carousel('to', 3);
 	});
 
 
